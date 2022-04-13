4fbb3f43ea4508b26c915e6bf082daafda29ffe4
nodari-dev1 <nodari.pylypyshak@gmail.com>
2022-04-13 22:32:02 +0200

    dashboard

---
+++ client/projects/dashboard/src/lib/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DashboardComponent
+  ]
+})
+export class DashboardModule { }
